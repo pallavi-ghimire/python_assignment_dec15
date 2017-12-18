eb352eb2469abce5d85e477388bed209ad5ffbd5
pallavi-ghimire <pallavi.ghimire.39@gmail.com>
2017-12-18 11:24:47 +0545

    Simply extracts "programming" out of the string

--- split.py
+++ split.py
@@ -2,7 +2,10 @@ string = input("Enter your full name: ")
 list1 = string.split()
 if(len(string)==2):
     (first_name, last_name) = list1
-    print(first_name, last_name)
+    print("first_name: ", first_name)
+    print("Last name: ", last_name)
 elif(len(string)==3):
     (first_name, middle_name, last_name) = list1
-    print(first_name, middle_name, last_name)
+    print("First_name: ",first_name)
+    print("Middle name: ",middle_name)
+    print("Last name: ",last_name)
